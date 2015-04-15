e71e136ea164a2d708c2ac50f0fa5966a6ea7fce
souravdatta <soura.jagat@gmail.com>
2015-04-15 18:30:22 +0530

    Space tab fiasco

--- lst.c
+++ lst.c
@@ -31,111 +31,111 @@ struct list  *create_list(void)
 struct list  *add_lnk_first(struct list *lst,
                             struct list_lnk *e)
 {
-	NOTNULL(e);
-	NOTNULL(lst);
+    NOTNULL(e);
+    NOTNULL(lst);
 	
-	if (lst->head == NULL) {
-		lst->head = lst->tail = e;		
-	}
-	else {
-		e->next = lst->head;
-		lst->head->prev = e;
-		lst->head = e;
-	}
-
-	return lst;
+    if (lst->head == NULL) {
+	lst->head = lst->tail = e;		
+    }
+    else {
+	e->next = lst->head;
+	lst->head->prev = e;
+	lst->head = e;
+    }
+    
+    return lst;
 }
 
 struct list  *rmv_list_lnk(struct list *lst,
                            struct list_lnk *e)
 {
-	NOTNULL(e);
-	NOTNULL(lst);
-	NOTNULL(lst->head);
-	NOTNULL(lst->tail);
-
-	if (lst->head == e) {
-		lst->head = e->next;
-		lst->head->prev = NULL;
-	}
-	else if (lst->tail == e) {
-		lst->tail = e->prev;
-		lst->tail->next = NULL;
-	}
-	else {
-		NOTNULL(e->prev);
-		NOTNULL(e->next);
-		e->prev->next = e->next;
-		e->next->prev = e->prev;
-	}
+    NOTNULL(e);
+    NOTNULL(lst);
+    NOTNULL(lst->head);
+    NOTNULL(lst->tail);
+    
+    if (lst->head == e) {
+	lst->head = e->next;
+	lst->head->prev = NULL;
+    }
+    else if (lst->tail == e) {
+	lst->tail = e->prev;
+	lst->tail->next = NULL;
+    }
+    else {
+	NOTNULL(e->prev);
+	NOTNULL(e->next);
+	e->prev->next = e->next;
+	e->next->prev = e->prev;
+    }
 	
-	e->next = e->prev = NULL;
-	return lst;
+    e->next = e->prev = NULL;
+    return lst;
 }
 
 struct list *add_lnk_after(struct list *lst,
-                            struct list_lnk *p,
-                            struct list_lnk *e)
+			   struct list_lnk *p,
+			   struct list_lnk *e)
 {
-	NOTNULL(e);
-	NOTNULL(p);
-	
-	e->next = p->next;
-	e->prev = p;
-	
-	if (p->next != NULL) {
-		p->next->prev = e;
-	}
-
-	p->next = e;
-	return lst;
+    NOTNULL(e);
+    NOTNULL(p);
+    
+    e->next = p->next;
+    e->prev = p;
+    
+    if (p->next != NULL) {
+	p->next->prev = e;
+    }
+    
+    p->next = e;
+    return lst;
 }
 
 struct list *add_lnk_last(struct list *lst,
-                           struct list_lnk *e)
+			  struct list_lnk *e)
 {
-	NOTNULL(lst);
-	NOTNULL(e);
-	
-	if (lst->tail == NULL) {
-		lst->head = lst->tail = e;
-	}
-	else {
-		lst->tail->next = e;
-		e->prev = lst->tail;
-		lst->tail = e;
-	}
-
-	return lst;
+    NOTNULL(lst);
+    NOTNULL(e);
+    
+    if (lst->tail == NULL) {
+	lst->head = lst->tail = e;
+    }
+    else {
+	lst->tail->next = e;
+	e->prev = lst->tail;
+	lst->tail = e;
+    }
+    
+    return lst;
 }
 
 void delete_list(struct list *lst)
 {
-	free(lst);
+    free(lst);
 }
 
 void list_each(struct list *lst,
-			   void (*fp)(void *arg))
+	       void (*fp)(void *arg))
 {
-	NOTNULL(lst);
-	NOTNULL(fp);
-
-	struct list_lnk *it = lst->head;
-	while (it != NULL) {
-		fp(it->parent);
-		it = it->next;
-	}
+    NOTNULL(lst);
+    NOTNULL(fp);
+    
+    struct list_lnk *it = lst->head;
+    while (it != NULL) {
+	fp(it->parent);
+	it = it->next;
+    }
 }
 
 void list_each_r(struct list *lst,
-			   void (*fp)(void *arg))
+		 void (*fp)(void *arg))
 {
-	NOTNULL(lst);
-	NOTNULL(fp);
+    NOTNULL(lst);
+    NOTNULL(fp);
 
-	struct list_lnk *it = lst->tail;
-	while (it != NULL) {
-		fp(it->parent);
-		it = it->prev;
-	}
+    struct list_lnk *it = lst->tail;
+    while (it != NULL) {
+	fp(it->parent);
+	it = it->prev;
+    }
 }
